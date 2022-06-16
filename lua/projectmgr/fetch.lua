f92cb253118c2fa24cc1cc1acbd4546e48e58d71
charludo <github@charlotteharludo.com>
2022-06-16 22:51:07 +0200

    temp

--- lua/projectmgr/fetch.lua
+++ lua/projectmgr/fetch.lua
@@ -14,7 +14,7 @@ function M.get_projects()
     local results = {}
 
     for i=1, nrow do
-        results[i] = {db_results.id[i], db_results.name[i], db_results.path[i], db_results.command[i]}
+        results[i] = tostring(db_results.id[i]) .. "  |  " .. db_results.name[i] .. " " .. db_results.path[i] .. " " .. db_results.command[i]
     end
 
     db:close()
--- lua/projectmgr/init.lua
+++ lua/projectmgr/init.lua
@@ -27,8 +27,8 @@ local function open_window()
     local height = api.nvim_get_option("lines")
 
     -- calculate our floating window size
-    local win_height = math.ceil(height * 0.8 - 4)
-    local win_width = math.ceil(width * 0.8)
+    local win_height = math.ceil(height * 0.5 - 4)
+    local win_width = math.ceil(width * 0.7)
 
     -- and its starting position
     local row = math.ceil((height - win_height) / 2 - 1)
@@ -82,14 +82,6 @@ local function update_view(direction)
     if position < 0 then position = 0 end
 
     local result = fetch.get_projects()
-    
-    local flattened = {}
-    for k,item in ipairs(result) do
-        flattened[k] = item[2]
-    end
-    for k,_ in pairs(flattened) do
-        flattened[k] = '  '..flattened[k]
-    end
 
     api.nvim_buf_set_lines(buf, 1, 2, false, {center('Projects')})
     api.nvim_buf_set_lines(buf, 3, -1, false, flattened)
