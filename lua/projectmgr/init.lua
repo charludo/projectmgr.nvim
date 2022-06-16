7d1a99b86d02c9c6c10ab79cfd31bbf6be584a0f
charludo <github@charlotteharludo.com>
2022-06-16 19:48:42 +0200

    missing import

--- lua/projectmgr/init.lua
+++ lua/projectmgr/init.lua
@@ -1,4 +1,5 @@
 -- Imports the plugin's additional Lua modules.
+local switch = require("projectmgr.switch")
 local fetch = require("projectmgr.fetch")
 local update = require("projectmgr.update")
 
@@ -9,7 +10,7 @@ local M = {}
 
 -- Routes calls made to this module to functions in the
 -- plugin's other modules.
-M.switch_project = fetch.switch_project
+M.switch_project = switch.switch_project
 M.get_projects = fetch.get_projects
 M.create_project = update.create_project
 M.delete_project = update.delete_project
