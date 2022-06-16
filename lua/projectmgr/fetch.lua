a5f3539ac1c41929e9003ba48853581ad15908e5
charludo <github@charlotteharludo.com>
2022-06-16 22:11:31 +0200

    temp

--- lua/projectmgr/fetch.lua
+++ lua/projectmgr/fetch.lua
@@ -10,7 +10,9 @@ function M.get_projects()
     local db = sqlite.open("projects.db")
 
     local db_results = db:exec("SELECT * FROM projects;")
+    for k, item in ipairs(db_results[1]) do print(k .. ' ' .. item) end
     for k, item in ipairs(db_results[2]) do print(k .. ' ' .. item) end
+    for k, item in ipairs(db_results[3]) do print(k .. ' ' .. item) end
     print(db_results)
     db:close()
     return db_results
