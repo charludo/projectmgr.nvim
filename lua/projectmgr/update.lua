f9861583410afed92e62aa310860b0690d0eb256
charludo <github@charlotteharludo.com>
2022-06-20 16:22:51 +0200

    added gif and cleaned db

--- lua/projectmgr/update.lua
+++ lua/projectmgr/update.lua
@@ -23,7 +23,7 @@ function M.create_project()
 
     local command = ""
     command = vim.fn.input("Startup Command (opional): ")
-    print("\nCreate new project.")
+    print("\rCreated new project.")
 
     local db = sqlite.open(db_path)
     db:exec("INSERT INTO projects (name, path, command) VALUES ('" .. name .. "', '" .. path .. "', '" .. command .. "');")
@@ -46,7 +46,7 @@ function M.update_project(old_name)
 
     local command = ""
     command = vim.fn.input("Startup Command (opional): ", old_command)
-    print("\nUpdated project.")
+    print("\rUpdated project.")
 
     local db = sqlite.open(db_path)
     db:exec("UPDATE projects SET name='"..name.."', path='"..path.."', command='"..command.."' WHERE name=='"..old_name.."';")
